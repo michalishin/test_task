6871edef5b129ba07a9a81d79199c81d41c34453
Vitalii Mikhalishin <vitalii.mikhalishin@creditexpress.ua>
2017-09-19 17:58:55 +0300

    create clients table

---
+++ database/migrations/2017_09_19_145041_create_clients_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->increments('id')->index();
+            $table->string('name',255);
+            $table->string('surname',255);
+            $table->char('sex');
+            $table->date('date_of_birth');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+}
